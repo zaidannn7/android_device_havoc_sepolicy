110b0ff5d4d84f28ddd1aa11571a246b33766bc8
Unknown <anushekprasal@gmail.com>
2018-05-08 01:13:28 +0530

    Init Havoc...

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from Havoc
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/havoc/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/havoc/sepolicy/qcom/common \
+    device/havoc/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
