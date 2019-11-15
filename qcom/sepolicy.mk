a0d6b43d6e054e9e7da97b473afad79aae5c97f7
theimpulson <aayushgupta219@gmail.com>
2019-11-15 03:45:40 +0000

    sepolicy: Label and address denials of livedisplay's system variant
    Change-Id: Ib7bf75607c46239cc7eee0785a548e7defbebada

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -14,7 +14,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/havoc/sepolicy/qcom/dynamic
+    device/havoc/sepolicy/qcom/dynamic \
+    device/havoc/sepolicy/qcom/system
 else
 BOARD_SEPOLICY_DIRS += \
     device/havoc/sepolicy/qcom/dynamic \
